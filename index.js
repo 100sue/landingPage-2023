2117c9fb6ac2aceefab31d095457507be8163c3c
appert <scoreur@gmail.com>
2023-08-13 11:46:51 +0200

    mise en place projet

---
+++ index.js
@@ -0,0 +1,51 @@
+
+function startloader() {
+    let counterElement = document.querySelector(".counter");
+    let currentValue = 0;
+    function updateCounter() {
+        if(currentValue === 100){
+            return;
+        }
+
+        currentValue += Math.floor(Math.random() * 10) + 1;
+
+        if(currentValue > 100) {
+            currentValue = 100;
+        }
+
+        counterElement.textContent = currentValue + "%";
+
+        let delay = Math.floor(Math.random() * 200) + 50;
+        setTimeout(updateCounter, delay);
+    }
+    updateCounter();
+}
+
+startloader();
+
+gsap.to(".counter-wrap h1", 1.5, {
+    delay: 3,
+    y:-320,
+    ease: "power4.inOut",
+    stagger: {
+        amount:0.5
+    },
+});
+
+gsap.to(".item h1, .item p", 1.5, {
+    delay: 3.5,
+    y:-40,
+    ease: "power4.inOut",
+    stagger: {
+        amount:0.5
+    },
+});
+
+gsap.to(".item-main h1", 1.5, {
+    delay: 3.5,
+    y:-280,
+    ease: "power4.inOut",
+    stagger: {
+        amount:0.5
+    },
+});
